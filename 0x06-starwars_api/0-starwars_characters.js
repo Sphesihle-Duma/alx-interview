b9cd7a95978abecbfe48711ab8bf8d9f5f40807d
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-02-18 18:51:51 +0200

    making changes

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,22 +1,46 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(url, async (err, response, body) => {
-  if (err) {
-    console.log(err);
-  }
-  for (const characterId of JSON.parse(body).characters) {
-    await new Promise((resolve, reject) => {
-      request(characterId, (err, response, body) => {
-        if (err) {
-          reject(err);
-        }
-        console.log(JSON.parse(body).name);
-        resolve();
-      });
-    });
-  }
+// Get the movie ID from command line argument
+const movieId = process.argv[2];
+
+// Make a GET request to the Star Wars API films endpoint
+request(`https://swapi.dev/api/films/${movieId}/`, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+    } else if (response.statusCode !== 200) {
+        console.error('Status:', response.statusCode);
+    } else {
+        const filmData = JSON.parse(body);
+        const charactersUrls = filmData.characters;
+        
+        // Array to store character names
+        const characters = [];
+
+        // Function to fetch character names recursively
+        const fetchCharacterNames = (urls, index) => {
+            if (index === urls.length) {
+                // All character names fetched, print them
+                characters.forEach(character => console.log(character));
+                return;
+            }
+            request(urls[index], (error, response, body) => {
+                if (error) {
+                    console.error('Error:', error);
+                } else if (response.statusCode !== 200) {
+                    console.error('Status:', response.statusCode);
+                } else {
+                    const characterData = JSON.parse(body);
+                    characters.push(characterData.name);
+                    // Fetch next character name
+                    fetchCharacterNames(urls, index + 1);
+                }
+            });
+        };
+
+        // Start fetching character names
+        fetchCharacterNames(charactersUrls, 0);
+    }
 });
+
